ed04c92ec87cb61768479387736de7ac9fcf4901
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-12 13:19:33 -0400

    Update analyze_players.py

--- analyze_players.py
+++ analyze_players.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 # Pull today's file. Static now but will be dynamic eventually
 def load_data(player_data.csv):
