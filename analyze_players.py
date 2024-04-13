e13a4809c052d0f1e556b7a3798101672c396feb
Sterling Ivey <149442241+SterlingIvey@users.noreply.github.com>
2024-04-13 07:56:27 -0700

    Update analyze_players.py

--- analyze_players.py
+++ analyze_players.py
@@ -1,7 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 
 # Pull today's file. Static now but will be dynamic
@@ -12,7 +9,7 @@ def preprocess_data(df):
     # Largest changes in player rankings from day to day and week to week
     df['day_change'] = abs(df['today_rank'] - df['yesterday_rank'])
     df['week_change'] = abs(df['today_rank'] - df['last_week_rank'])
-    df = df.dropna(subset=['today_rank', 'yesterday_rank', 'last_week_rank', 'score']), inplace=True
+    df.dropna(subset=['today_rank', 'yesterday_rank', 'last_week_rank', 'score'])
     return df
 
 def largest_change_players(df):
